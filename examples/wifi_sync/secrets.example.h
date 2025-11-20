55845ee6b50a95842c4fdacfe8dffdb7b5888597
Aman Tewary <aman.tewary@live.com>
2025-11-20 09:03:46 -0500

    fix: move WiFi credentials to secrets.h

---
+++ examples/wifi_sync/secrets.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Fill this in and rename to secrets.h (not tracked by git)
+#define WIFI_SSID      "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD  "YOUR_WIFI_PASSWORD"
+
+#define HOST_NAME      "lilygo"
+
--- examples/wifi_sync/wifi_sync.ino
+++ examples/wifi_sync/wifi_sync.ino
@@ -34,6 +34,7 @@
 #include <FFat.h>
 #include "logo.h"
 #include "utilities.h"
+#include "secrets.h"
 
 
 // #define USE_SD
@@ -47,9 +48,9 @@
 
 #define DBG_OUTPUT_PORT Serial
 
-const char *ssid = "Artemis_2.4GEXT";
-const char *password = "19E6942496D6";
-const char *host = "lilygo";
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+const char *host = HOST_NAME;
 
 WebServer server(80);
 static bool hasFILE_SYSTEM = false;
